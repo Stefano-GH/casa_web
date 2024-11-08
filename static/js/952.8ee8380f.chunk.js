3b7a95c55ec1a8df12f53727795e0cb941208cfa
Stefano Tumino <TuminoS@int.master.lan>
2024-11-08 12:13:44 +0100

    Updates

---
+++ static/js/952.8ee8380f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcasa_web=self.webpackChunkcasa_web||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.8ee8380f.chunk.js.map
